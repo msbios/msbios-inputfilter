86bc0dc292f321ba542217b0dab528bc04b17118
Judzhin Miles <info@msbios.com>
2019-09-27 21:31:42 +0300

    Lazy Composer MSBios Commit Script

--- src/Module.php
+++ src/Module.php
@@ -12,7 +12,7 @@ namespace MSBios\InputFilter;
 class Module extends \MSBios\Module
 {
     /** @const VERSION */
-    const VERSION = '1.0.6';
+    const VERSION = '1.0.7';
 
     /**
      * @inheritdoc
